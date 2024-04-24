bc2c36bd2c9917b76c46d0c294af48460707ebec
Sergio Loza <sergiolozav@gmail.com>
2024-04-24 04:28:45 -0400

    fix: Margen top era mucho

--- src/modules/auth/components/AuthContainer.tsx
+++ src/modules/auth/components/AuthContainer.tsx
@@ -6,7 +6,7 @@ type AuthContainerProps = {
 
 export function AuthContainer(props: AuthContainerProps) {
   return (
-    <div className="mb-12 flex lg:mt-44 justify-center px-4 lg:w-6/12 lg:px-12">
+    <div className="mb-12 flex lg:mt-24 justify-center px-4 lg:w-6/12 lg:px-12">
       <div className="w-full max-w-96">{props.children}</div>
     </div>
   );
