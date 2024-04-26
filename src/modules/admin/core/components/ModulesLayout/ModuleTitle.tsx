b023c7263a4f5acf5abee4973f6286d816648196
Sergio Loza <sergiolozav@gmail.com>
2024-04-26 00:44:48 -0400

    feat: Ajustes menores de UI

--- src/modules/admin/core/components/ModulesLayout/ModuleTitle.tsx
+++ src/modules/admin/core/components/ModulesLayout/ModuleTitle.tsx
@@ -6,7 +6,7 @@ type ModuleTitleProps = {
 
 export function ModuleTitle(props: ModuleTitleProps) {
   return (
-    <div className="flex text-foreground/90">
+    <div className="mt-2 flex text-foreground/90 md:mt-0">
       <div className="mt-1 flex h-9 items-end p-1 md:hidden">
         <button className="pointer-events-auto rounded-md px-1 py-0.5 text-2xl hover:bg-foreground/10">
           <MdMenu />
--- src/modules/auth/components/BorderLink.tsx
+++ src/modules/auth/components/BorderLink.tsx
@@ -7,7 +7,7 @@ export function BorderLink(props: BorderLinkProps) {
   return (
     <Link
       to={props.to}
-      className="rounded border-2 border-primary-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-neutral-950/10"
+      className="rounded border-2 border-primary/80 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-accent"
     >
       {props.children}
     </Link>
