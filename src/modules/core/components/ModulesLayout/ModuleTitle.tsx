cd1624cbabb9a40df91abe7902570a66b28ece6e
Sergio Loza <sergiolozav@gmail.com>
2024-05-01 22:48:37 -0400

    fix: Mejor espaciado título de módulo

--- src/modules/core/components/ModulesLayout/ModuleTitle.tsx
+++ src/modules/core/components/ModulesLayout/ModuleTitle.tsx
@@ -8,7 +8,7 @@ export function ModuleTitle(props: ModuleTitleProps) {
   return (
     <div className="mt-2 flex text-foreground/90 md:mt-0">
       <div className="mt-1 flex h-9 items-end p-1 md:hidden">
-        <button className="pointer-events-auto rounded-md px-1 py-0.5 text-2xl hover:bg-foreground/10">
+        <button className="-ml-2 pointer-events-auto rounded-md px-1 py-0.5 text-2xl hover:bg-foreground/10">
           <MdMenu />
         </button>
       </div>
