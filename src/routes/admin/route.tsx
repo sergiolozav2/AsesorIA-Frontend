de82a830fe2d6eba26da78a00ad529f33386d2f6
Sergio Loza <sergiolozav@gmail.com>
2024-05-01 22:29:28 -0400

    feat: Protección de rutas en /admin/

--- src/routes/admin/route.tsx
+++ src/routes/admin/route.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { ApplicationSidebar } from "../../modules/core/components/ApplicationSidebar";
+import { RequiredLoginWrapper } from "@/modules/core/components/RequiredLoginWrapper";
 
 export const Route = createFileRoute("/admin")({
   component: AppLayout,
@@ -7,12 +8,12 @@ export const Route = createFileRoute("/admin")({
 
 function AppLayout() {
   return (
-    <>
+    <RequiredLoginWrapper>
       <ApplicationSidebar />
       <div className="flex">
         <div className="mr-0 transition-all md:mr-admin-navbar"></div>
         <Outlet />
       </div>
-    </>
+    </RequiredLoginWrapper>
   );
 }
