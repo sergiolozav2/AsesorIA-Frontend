bf437936823807caf1046509fb0e41e9e4a88770
Sergio Loza <sergiolozav@gmail.com>
2024-05-02 17:28:39 -0400

    style: prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import path from "path"
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
